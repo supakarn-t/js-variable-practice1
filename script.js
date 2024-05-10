135cae54add95699612485c6aa7c736e1501f439
Supakarn Tangsirivatthanavong <hikaru8889@gmail.com>
2024-05-10 15:19:13 +0700

    practice

--- script.js
+++ script.js
@@ -1,3 +1,52 @@
+var var_out = "var out";
+let let_out = "let out";
+const const_out = "const out";
 
+// Block Scope
 
+{
+  var var_block = "var block";
+  let let_block = "let block";
+  const const_block = "const block";
 
+  console.log(var_out);
+  console.log(let_out);
+  console.log(const_out);
+
+  console.log(var_block);
+  console.log(let_block);
+  console.log(const_block);
+}
+
+// Function Scope
+function my_stock(name) {
+  var var_func = name;
+  let let_func = name;
+  const const_func = name;
+
+  console.log(var_out);
+  console.log(let_out);
+  console.log(const_out);
+
+  console.log(var_block);
+  //   console.log(let_block);
+  //   console.log(const_block);
+
+  console.log(var_func);
+  console.log(let_func);
+  console.log(const_func);
+}
+
+my_stock("func");
+
+console.log(var_out);
+console.log(let_out);
+console.log(const_out);
+
+console.log(var_block);
+// console.log(let_block);
+// console.log(const_block);
+
+console.log(var_func);
+// console.log(let_func);
+// console.log(const_func);
